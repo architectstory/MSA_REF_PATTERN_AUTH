8764d7deea5019b9ecd62ebc768e722c4ddd480c
archiectstory <architectstory@gmail.com>
2024-03-05 18:42:46 +0900

    add spring security sample

---
+++ SPRING_SECURITY_SAMPLE/src/main/java/com/samsungsds/msa/auth/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.samsungsds.msa.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+
+}
---
+++ SPRING_SECURITY_SAMPLE/src/main/java/com/samsungsds/msa/auth/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.samsungsds.msa.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //To enable Spring Security’s web security support and provide the Spring MVC integration.
+public class WebSecurityConfig {
+
+	/**
+	 * SecurityFilterChain bean defines which URL paths should be secured and which should not.
+	 * Specifically, the / and /home paths are configured to not require any authentication.
+	 * All other paths must be authenticated.
+	 * @param http
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				.loginPage("/login")
+				.permitAll()
+			)
+			.logout((logout) -> logout.permitAll());
+
+		return http.build();
+	}
+
+	/**
+	 * The UserDetailsService bean sets up an in-memory user store with a single user.
+	 * That user is given a username of user, a password of password, and a role of USER.
+	 * @return
+	 */
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
