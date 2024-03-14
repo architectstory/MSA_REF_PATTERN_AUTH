d1790debfc764446b1c293d4e34b1383544775e2
archiectstory <architectstory@gmail.com>
2024-03-14 19:05:03 +0900

    for edu

--- MSA_REF_AUTH/src/main/java/com/samsungsds/msa/auth/adapter/AuthenticationAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.samsungsds.msa.auth.adapter;
-
-public interface AuthenticationAdapter<Request, Response> {
-    Response initiateAuthentication(Request request) throws Exception;
-    Response authorization(Request request) throws Exception;
-}
--- MSA_REF_AUTH/src/main/java/com/samsungsds/msa/auth/adapter/SecurityIgnoring.java
+++
@@ -1,8 +0,0 @@
-package com.samsungsds.msa.auth.adapter;
-
-import lombok.Data;
-
-@Data
-public class SecurityIgnoring {
-    private String[] urlAntPatterns;
-}
